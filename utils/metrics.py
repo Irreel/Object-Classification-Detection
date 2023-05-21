c690daae449f9f239243662c02d85eea7553d662
Irreel <phantasy0@qq.com>
2023-05-21 16:49:07 +0800

    Implement valid metrics

---
+++ utils/metrics.py
@@ -0,0 +1,8 @@
+import torch
+
+
+def topKAcc(logits, label, K= 5):
+    _, pred = torch.topk(logits, k=K)
+    num_correct += torch.sum(torch.eq(pred, label.view(-1, 1))).item()
+    
+    return num_correct / label.size(0)
