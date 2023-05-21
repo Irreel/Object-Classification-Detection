2ae72811364e27237d7b71700854dd23802d5530
Irreel <phantasy0@qq.com>
2023-05-21 17:00:03 +0800

    Debug

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,6 +3,6 @@ import torch
 
 def topKAcc(logits, label, K= 5):
     _, pred = torch.topk(logits, k=K)
-    num_correct += torch.sum(torch.eq(pred, label.view(-1, 1))).item()
+    num_correct = torch.sum(torch.eq(pred, label.view(-1, 1))).item()
     
     return num_correct / label.size(0)
